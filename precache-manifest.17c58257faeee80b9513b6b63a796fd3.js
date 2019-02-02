43ed96135fa6230685621929475e39e4736a88c1
Dilum Sanjaya <dilumsanjaya@gmail.com>
2019-02-02 18:15:59 +0530

    Updates

---
+++ precache-manifest.17c58257faeee80b9513b6b63a796fd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d87f4b99bbedc3ceaf",
+    "url": "/questionnaires/static/js/runtime~main.74d87f4b.js"
+  },
+  {
+    "revision": "ca486c7210803dad00dc",
+    "url": "/questionnaires/static/js/main.ca486c72.chunk.js"
+  },
+  {
+    "revision": "2efc9dfd099e1ca0e8a3",
+    "url": "/questionnaires/static/js/1.2efc9dfd.chunk.js"
+  },
+  {
+    "revision": "ca486c7210803dad00dc",
+    "url": "/questionnaires/static/css/main.07d82d4f.chunk.css"
+  },
+  {
+    "revision": "db2f17586b8651ed021739a6b3899f80",
+    "url": "/questionnaires/index.html"
+  }
+];
